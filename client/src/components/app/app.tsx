1ef79f49e179f099fe445c6d151087dc2b9c420a
Braden Borman <bradenborman@hotmail.com>
2023-06-13 17:02:06 -0500

    Updated gitignore

---
+++ client/src/components/app/app.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+require('./app.scss');
+
+const App: React.FC = () => {
+
+    return (
+        <>
+            <Router>
+               
+            </Router>
+        </>
+    );
+};
+
+export default App;
